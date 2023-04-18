409b976ffcb45e787115d2c70d69251c568c70e7
Amanda Reyes <areyes4368@stu.d214.org>
2023-04-18 11:19:54 -0500

    Create project

--- Assignment_Notebook/ViewController.swift
+++ Assignment_Notebook/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDataSource , UITableViewDelegate {
+    var assignments : [String] = []
+    @IBOutlet weak var assignmentTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        assignmentTable.dataSource = self
+        assignmentTable.delegate = self
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return assignments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = assignmentTable.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = assignments[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    @IBAction func whenAdded(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Task", message: "Add your task below", preferredStyle: .alert)
+        alert.addTextField()
+        var task = (alert.textFields?[0].text)!
+        let add = UIAlertAction(title: "Add", style: .default, handler: {action in self.assignments.append(task)
+            self.assignmentTable.reloadData()
+        })
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(add)
+        alert.addAction(cancel)
+        present(alert, animated: true)
+    }
+    
 
 }
 
